a907056ab559ec4b3bd0e1af91a584d8d403d846
ivvasch <56225016+ivvasch@users.noreply.github.com>
2019-11-12 15:11:44 +0300

    Create PersonScanner.java

---
+++ adapter/PersonScanner.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public interface PersonScanner {
+    Person read() throws IOException, ParseException;
+
+    void close() throws IOException;
+}
